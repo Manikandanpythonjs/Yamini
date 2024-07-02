e38496926bfc76d6d736a4c9c6c61bc84bd4d663
Manikandanpythonjs <89934919+Manikandanpythonjs@users.noreply.github.com>
2024-07-02 13:50:28 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+const colors = require('tailwindcss/colors')
+const { default: flattenColorPalette } = require('tailwindcss/lib/util/flattenColorPalette')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
+  ttheme: {
+    extend: {
+      animation: {
+        shimmer: 'shimmer 2s linear infinite'
+      },
+      keyframes: {
+        shimmer: {
+          from: {
+            backgroundPosition: '0 0'
+          },
+          to: {
+            backgroundPosition: '-200% 0'
+          }
+        }
+      }
+    }
+  }
+}
+
+function addVariablesForColors({ addBase, theme }) {
+  let allColors = flattenColorPalette(theme('colors'))
+  let newVars = Object.fromEntries(Object.entries(allColors).map(([key, val]) => [`--${key}`, val]))
+
+  addBase({
+    ':root': newVars
+  })
+}
